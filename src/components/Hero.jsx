c3fc4d72d4bb66a461ce7c3abcd65cdf05bb73bb
Mohamed Islaam K A <mohamedislaam87@gmail.com>
2025-07-25 14:10:28 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,9 +10,9 @@ function Hero() {
         <p className="year">Final year student</p>
         <a href="#about" className="abt">About me</a>
         <ul>
-          <li><a href="https://www.instagram.com/mhd_islaam?igsh=aTZpem1pNDhyZGhv" target="_blank" rel="noopener noreferrer"><img width="48" height="48" src="https://img.icons8.com/material-outlined/48/a3a3a3/instagram-new--v1.png" alt="instagram-new" /></a></li>
-          <li><a href="http://linkedin.com/in/mohamed-islaam-5883b4255/" target="_blank" rel="noopener noreferrer"><img width="48" height="48" src="https://img.icons8.com/ios-glyphs/48/a3a3a3/linkedin.png" alt="linkedin" /></a></li>
-          <li><a href="https://github.com/MohamedIslaam" target="_blank" rel="noopener noreferrer"><img width="48" height="48" src="https://img.icons8.com/ios-glyphs/48/a3a3a3/github.png" alt="github" /></a></li>
+          <li><a href="https://www.instagram.com/mhd_islaam?igsh=aTZpem1pNDhyZGhv" target="_blank" rel="noopener noreferrer"><img width="48" height="48" src="https://img.icons8.com/material-outlined/56/a3a3a3/instagram-new--v1.png" alt="instagram-new" /></a></li>
+          <li><a href="http://linkedin.com/in/mohamed-islaam-5883b4255/" target="_blank" rel="noopener noreferrer"><img width="48" height="48" src="https://img.icons8.com/ios-glyphs/56/a3a3a3/linkedin.png" alt="linkedin" /></a></li>
+          <li><a href="https://github.com/MohamedIslaam" target="_blank" rel="noopener noreferrer"><img width="48" height="48" src="https://img.icons8.com/ios-glyphs/56/a3a3a3/github.png" alt="github" /></a></li>
         </ul>
       </div>
 
